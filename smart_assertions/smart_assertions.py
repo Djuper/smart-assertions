39ba2fdc57af355348147bc0524bf169b61ab00a
Djuper <behalserhii@gmail.com>
2022-06-15 23:18:23 +0300

    [add] xfail logic

--- smart_assertions/smart_assertions.py
+++ smart_assertions/smart_assertions.py
@@ -1,26 +1,37 @@
 import types
 import inspect
+import pytest
+
 
 failed_conditions = []
+xfailed_conditions = []
 
 
-def soft_assert(assert_condition, message=None):
+def soft_assert(assert_condition, message=None, xfail=False):
     global failed_conditions
+    global xfailed_conditions
     if isinstance(assert_condition, types.FunctionType):
         try:
             assert_condition()
         except AssertionError as error:
-            add_exception(message if message else error)
+            if xfail:
+                add_xfailed(message if message else error)
+            else:
+                add_exception(message if message else error)
     else:
         try:
             assert assert_condition
         except AssertionError:
-            add_exception(message if message else 'Failed by assertion!')
+            if xfail:
+                add_xfailed(message if message else error)
+            else:
+                add_exception(message if message else 'Failed by assertion!')
 
 
 def verify_expectations():
     global failed_conditions
-    if failed_conditions:
+    global xfailed_conditions
+    if failed_conditions or xfailed_conditions:
         report_exceptions()
 
 
@@ -30,8 +41,17 @@ def add_exception(message=None):
     failed_conditions.append('Exception: {}\nFail in "{}:{}" {}()\n'.format(message, file_path, line, function_name))
 
 
+def add_xfailed(message=None):
+    global xfailed_conditions
+    (file_path, line, function_name) = inspect.stack()[2][1:4]
+    xfailed_conditions.append('Exception: {}\nFail in "{}:{}" {}()\n'.format(message, file_path, line, function_name))
+
+
 def report_exceptions():
     global failed_conditions
+    global xfailed_conditions
+    report = []
+    xreport = []
     if failed_conditions:
         report = ['Failed conditions count: [ {} ]\n'.format(len(failed_conditions))]
         for index, failure in enumerate(failed_conditions, start=1):
@@ -40,4 +60,16 @@ def report_exceptions():
             else:
                 report.append(failure)
         failed_conditions = []
+    if xfailed_conditions:
+        xreport = ['XFailed conditions count: [ {} ]\n'.format(len(xfailed_conditions))]
+        for index, failure in enumerate(xfailed_conditions, start=1):
+            if len(xfailed_conditions) > 1:
+                xreport.append('{}. {}'.format(index, failure))
+            else:
+                xreport.append(failure)
+        xfailed_conditions = []
+    if report:
+        report += xreport
         raise AssertionError('\n'.join(report))
+    elif xreport:
+        pytest.xfail('\n'.join(xreport))
